e78121eb7ffe2338a5d640cbf766ae45f13c3b75
lee22br <leeiglezias@gmail.com>
2023-03-07 17:58:22 -0300

    Pega uma pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,19 @@ class PessoaController{
             return res.status(500).json(error.message);
         }
     }
+    static async pegaUmaPessoa (req, res){
+        const { id } = req.params;
+        try{
+            const umaPessoa = await database.findOne( {
+                where: {
+                    id: Number(id)
+                }
+            } );
+            return res.status(200).json(umaPessoa);
+        }catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,5 +3,6 @@ const PessoaController = require ('../controllers/PessoaController');
 
 const router = Router();
 router.get('/pessoas', PessoaController.pegaTodasAsPessoa);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
 module.exports = router;
