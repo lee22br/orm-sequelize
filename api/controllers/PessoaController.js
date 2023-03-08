28c9e841b026f22278d56b4804232ada0103900f
lee22br <leeiglezias@gmail.com>
2023-03-08 17:38:12 -0300

    Cria Pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,7 @@ class PessoaController{
             return res.status(500).json(error.message);
         }
     }
+    
     static async pegaUmaPessoa (req, res){
         const { id } = req.params;
         try{
@@ -22,6 +23,17 @@ class PessoaController{
             return res.status(500).json(error.message);
         }
     }
+
+    static async criaPessoa (req, res){
+        const novaPessoa = req.body;
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require ('../controllers/PessoaController');
 const router = Router();
 router.get('/pessoas', PessoaController.pegaTodasAsPessoa);
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
 
 module.exports = router;
