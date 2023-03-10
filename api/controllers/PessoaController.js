501a2a3b350ba7032cf518ccb4b1af7e97ab904b
lee22br <leeiglezias@gmail.com>
2023-03-10 17:28:01 -0300

    atualiza matricula

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -84,6 +84,24 @@ class PessoaController{
         }
     }
 
+    static async atualizaMatricula (req, res){
+        const { estudanteId, matriculaId } = req.params;
+        const novasInfos = req.body;
+        try {
+            await database.Matriculas.update(novasInfos, {
+                where: {
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId)
+                }
+            });
+            const matriculaAtualizada = await database.Matriculas.findByPk(matriculaId);
+            res.status(200).json(matriculaAtualizada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+            
+        }
+    }
+
 }
 
 module.exports = PessoaController
