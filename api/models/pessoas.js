7568a9798113784ff792470d3956c52f8f6e4436
lee22br <leeiglezias@gmail.com>
2023-03-13 15:00:37 -0300

    pessoas modelo padrão ativo

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -22,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     paranoid: true,
+    defaultScope: { where: { ativo: true }},
     modelName: 'Pessoas',
   });
   return Pessoas;
