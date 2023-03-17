77eec2017419d3e3942bf64867d83901aaa92639
lee22br <leeiglezias@gmail.com>
2023-03-17 18:20:21 -0300

    Criação PessoasServices

--- api/services/PessoasServices.js
+++ api/services/PessoasServices.js
@@ -18,7 +18,7 @@ class PessoasServices extends Services{
     async cancelaPessoaEMatriculas(estudanteId){
         return database.sequelize.transacation(async transacao => {
             await super.atualizaRegistro({ativo: false},estudanteId, { transacation: transacao})
-            await this.matriculas.atualizaRegistro({status: 'cancelado'}, {estudante_id: estudanteId}, {transacation: transacao})
+            await this.matriculas.atualizaRegistros({status: 'cancelado'}, {estudante_id: estudanteId}, {transacation: transacao})
         });
     }
 
