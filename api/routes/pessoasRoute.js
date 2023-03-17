1b5070640810986b36cf6f6e0738e834d48df53e
lee22br <leeiglezias@gmail.com>
2023-03-17 19:56:23 -0300

    finalizando

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,15 +8,8 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 router.post('/pessoas', PessoaController.criaPessoa);
 router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.deletaPessoa);
-router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
-router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
 router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas);
-router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletaMatricula);
 router.post('/pessoas/:id', PessoaController.restauraPessoa);
-router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
-router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma);
-router.get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas);
 router.put('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa);
 
 module.exports = router;
