7da110dc0e0452c8468f33f8db568681d9dcabe1
lee22br <leeiglezias@gmail.com>
2023-03-17 18:17:11 -0300

    Criação PessoasServices

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -200,15 +200,14 @@ class PessoaController{
     static async cancelaPessoa (req, res){
         const { estudanteId } = req.params;
         try {
-            database.sequelize.transaction(async transacao => {
-                await database.Pessoas.update({ativo: false}, {where: {id: Number(estudanteId)}}, {transaction: transacao});
-                await database.Matriculas.update({status: 'cancelado'}, {where:{estudante_id: Number(estudanteId)}},{transaction: transacao});
+                await pessoasServices.cancelaPessoaEMatriculas(Number(estudanteId));
                 return res.status(200).json({menssage: `matriculas ref. ao estudante ${estudanteId} cancelada(s)`});
-            })
-        } catch (error) {
+            }
+        catch (error) {
             return res.status(500).json(error.message);
         }
     }
+
 }
 
 module.exports = PessoaController
--- api/services/PessoasServices.js
+++ api/services/PessoasServices.js
@@ -4,6 +4,7 @@ const database = require ('../models');
 class PessoasServices extends Services{
     constructor(){
         super('Pessoas');
+        this.matriculas = new Services('Matriculas');
     }
 
     async pegaRegistrosAtivos(where = {}){
@@ -12,6 +13,13 @@ class PessoasServices extends Services{
 
     async pegaTodosOsRegistros(where = {}){
         return database[this.nomeDoModelo].scope('todos').findAll({where: {...where}});
+    }
+
+    async cancelaPessoaEMatriculas(estudanteId){
+        return database.sequelize.transacation(async transacao => {
+            await super.atualizaRegistro({ativo: false},estudanteId, { transacation: transacao})
+            await this.matriculas.atualizaRegistro({status: 'cancelado'}, {estudante_id: estudanteId}, {transacation: transacao})
+        });
     }
 
 }
