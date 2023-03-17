4b65ba26686431527706a160f49e661d31230d92
lee22br <leeiglezias@gmail.com>
2023-03-17 19:34:12 -0300

    finalizando

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -25,7 +25,7 @@ class PessoaController{
     static async pegaUmaPessoa (req, res){
         const { id } = req.params;
         try{
-            const umaPessoa = await pessoasServices.pegaUmRegistro(id);
+            const umaPessoa = await pessoasServices.pegaUmRegistro({id});
             return res.status(200).json(umaPessoa);
         }catch (error){
             return res.status(500).json(error.message);
